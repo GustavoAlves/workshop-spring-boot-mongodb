766f599347ecf1e1a51da347428167d148ac1d87
Gustavo Alves <gustavo_alves@outlook.com>
2018-03-10 20:26:31 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/gustavoalves/workshopmongo/domain/User.java
+++ src/main/java/com/gustavoalves/workshopmongo/domain/User.java
@@ -2,11 +2,17 @@ package com.gustavoalves.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
-	private String nome;
+	
+	private String name;
 	private String email;
 	
 	public User() {
@@ -14,7 +20,7 @@ public class User implements Serializable {
 	
 	public User(String id, String nome, String email) {
 		this.id = id;
-		this.nome = nome;
+		this.name = nome;
 		this.email = email;
 	}
 
@@ -25,10 +31,10 @@ public class User implements Serializable {
 		this.id = id;
 	}
 	public String getNome() {
-		return nome;
+		return name;
 	}
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.name = nome;
 	}
 	public String getEmail() {
 		return email;
